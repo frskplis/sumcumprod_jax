92c81084e31bce93a30df31f659bcaf887e7d233
frskplis <kontakt@piotrlis.pl>
2023-07-28 17:21:30 +0000

    correcting

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
 cd /github/workspace
-KEPLER_JAX_CUDA=yes python3 -m pip install .
+SUMCUMPROD_JAX_CUDA=yes python3 -m pip install .
 python3 -c 'import sumcumprod_jax;print(sumcumprod_jax.__version__)'
 python3 -c 'from sumcumprod_jax import gpu_ops'
