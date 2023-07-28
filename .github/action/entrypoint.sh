8c548972591e8897268d31525235f3ce5f2166b1
frskplis <kontakt@piotrlis.pl>
2023-07-28 17:12:41 +0000

    correcting tests

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -2,5 +2,5 @@
 
 cd /github/workspace
 KEPLER_JAX_CUDA=yes python3 -m pip install .
-python3 -c 'import kepler_jax;print(kepler_jax.__version__)'
-python3 -c 'import kepler_jax.gpu_ops'
+python3 -c 'import sumcumprod_jax;print(sumcumprod_jax.__version__)'
+python3 -c 'import sumcumprod_jax.gpu_ops'
